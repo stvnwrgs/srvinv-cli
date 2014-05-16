43ced30df71c23ae24e0b469c63e04697746c1f4
Nils Bartels <nils.bartels@clt106153.collins.kg>
2014-05-16 18:22:41 +0200

    [ADD] further stuff;

--- libsrvinv/__init__.py
+++ libsrvinv/__init__.py
@@ -5,6 +5,7 @@ libsrvinv - the main library serving default methods for srvinv clients
 import config
 import requests
 import json
+from datetime import datetime
 
 api_url = config.master_url + config.api_version + '/'
 
@@ -22,13 +23,36 @@ def get(resource, resourceid, attribute):
     print 'error communicating with srvinv daemon'
 
 def set(resource, resourceid, attribute, value):
-  print resource
+  apirequest = requests.get(api_url + resource + 's/' + resourceid)
+  if apirequest.status_code == 200:
+      resource_as_obj = json.loads(apirequest.text)
+      resource_as_obj[attribute] = value
+      to_set_resource = json.dumps(resource_as_obj)
+      apirequest = requests.put(api_url + resource + 's/' + resourceid, data=to_set_resource)
+      if apirequest.status_code == 202:
+        print to_set_resource
+      else:
+        print 'error communicating with srvinv daemon'
+  elif apirequest.status_code == 404:
+    print 'resource not found'
+  elif apirequest.status_code == 500:
+    print 'error communicating with srvinv daemon'
 
 def register(resource, resourceid):
-  print resource
+  to_register_resource = {"name": resourceid, "created_at": unicode(datetime.utcnow())}
+  to_register_resource = json.dumps(to_register_resource)
+  apirequest = requests.post(api_url + resource + 's', data=to_register_resource)
+  if apirequest.status_code == 201:
+    print to_register_resource
+  else:
+    print 'error communicating with srvinv daemon'
 
 def delete(resource, resourceid):
-  print resource
+  apirequest = requests.delete(api_url + resource + 's/' + resourceid)
+  if apirequest.status_code == 202:
+    print 'deleted ' + resource + ': ' + resourceid
+  else:
+    print 'error communicating with srvinv daemon'
 
 def search(resource, attribute, value):
   print resource
